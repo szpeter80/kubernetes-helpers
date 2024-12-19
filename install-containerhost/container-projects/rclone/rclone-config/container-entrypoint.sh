28f83c8d7085fbc2f40c63486b38a510e814a314
Peter <szpeter80@users.noreply.github.com>
2024-12-19 16:45:24 +0100

    ~ [install-containerhost] update container-entrypoint.sh to react to TERM signal and handle the started processes as well

--- install-containerhost/container-projects/rclone/rclone-config/container-entrypoint.sh
+++ install-containerhost/container-projects/rclone/rclone-config/container-entrypoint.sh
@@ -1,22 +1,42 @@
 #!/bin/sh
-# TODO: debug why (SIG)TERM at podman pod rm is not working, maybe an exit is missing in _term ?
+# We are PID 1 so lets behave responsibly
+# - set up TERM signal handlers and take care of all processes we started
+# - run programs in the background because shell does not process signals 
+#   if a foreground process is running 
+# - check in the main loop if we need to exit
 
 
-_term() { 
-  echo "Caught SIGTERM signal!" 
+_term() {
+
+  TERM_RECEIVED="Y"
+  date
+  
+  echo "ENTRYPOINT: starting process shutdown" 
   killall -TERM sleep 2>/dev/null
   killall -TERM rclone 2>/dev/null
+  
+  echo "ENTRYPOINT: signals sent, waiting for subprocesses to exit"
+  wait
+  
+  echo "ENTRYPOINT: all processes have finished"
 }
 
-trap _term TERM
+trap _term HUP INT QUIT TERM
 
 ###############################################################################
 
-if [ ! -f /usr/bin/sleep ];
+TERM_RECEIVED="N"
+
+echo 
+echo "ENTRYPOINT: starting up the container"
+
+if [ ! -f /bin/sleep ];
 then
 
+    echo "ENTRYPOINT: starting initial system configuration"
+
     apk update
-    apk add coreutils bash bash-completion
+    apk add coreutils bash bash-completion nano
 
     # creates completion file on the filesystem
     rclone completion bash
@@ -38,6 +58,14 @@ fi;
 
 while true;
 do
+
+
+	if [[ "${TERM_RECEIVED}" == "Y" ]]; then
+	  echo "ENTRYPOINT: termination request received in main loop, exiting"
+	  date
+	  exit 0
+	fi
+	
     DT="$(date '+%Y-%m-%d_%H%M')"
       
     BDIR=$(dirname "${RCLONE_REPORT_FN}")
@@ -47,11 +75,16 @@ do
     echo "$(date '+%Y-%m-%d %H:%M')    executing rclone" | tee -a "${REPORT_FN}"
     echo "$(date '+%Y-%m-%d %H:%M')    RCLONE_CMD= $RCLONE_CMD" | tee -a "${REPORT_FN}"
     echo >> "${REPORT_FN}"
+    
     # combined report truncates the file so we need to use stdout and redirect to append to it
-    ${RCLONE_CMD} --combined - >> "${REPORT_FN}" && touch "${RCLONE_HEARTBEAT_FN}"
+    { ${RCLONE_CMD} --combined - >> "${REPORT_FN}" && touch "${RCLONE_HEARTBEAT_FN}"; } &
+    wait $!
+    
+    
     echo "$(date '+%Y-%m-%d %H:%M')    execution finished" | tee -a "${REPORT_FN}"
     echo
 
     # safety sleep, to avoid extreme load generation if RCLONE_SLEEP is missing or malformed
-    sleep "${RCLONE_SLEEP}"  ||  sleep 5
+    { sleep "${RCLONE_SLEEP}"  ||  sleep 5 ; } &
+    wait $!
 done;
